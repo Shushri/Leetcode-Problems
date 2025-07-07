a705a4debc0efc39f7cf3c1c2616a6e8b9baec65
Shrishti Mishra <shubhiarnm@gmail.com>
2025-07-07 23:51:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.03%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-
-        // Handle negative exponent
-        if (n < 0) {
-            x = 1 / x;
-            // To handle edge case when n == INT_MIN
-            if (n == INT_MIN) {
-                return x * myPow(x, -(n + 1));
+        if(n==0){
+            return 1;
+        }
+        long long int nn=n;//to handle INT_MIN
+        double ans=1.0;
+        if(nn<0){
+            nn=-1*nn;
+        }
+        while(nn){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
             }
-            n = -n;
+            else{
+                ans=ans*x;
+                nn=nn-1;
+            }
         }
-
-        double half = myPow(x, n / 2);
-
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return x * half * half;
+        if(n<0){
+            return (double)(1.0)/(double)(ans);
         }
+        return ans;
     }
 };
