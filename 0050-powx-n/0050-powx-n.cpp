537109ab0c6f18c500792bfc4b4ad714d4988054
Shrishti Mishra <shubhiarnm@gmail.com>
2025-07-08 00:03:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.64%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,24 +4,18 @@ public:
         if(n==0){
             return 1;
         }
-        long long int nn=n;//to handle INT_MIN
-        double ans=1.0;
-        if(nn<0){
-            nn=-1*nn;
-        }
-        while(nn){
-            if(nn%2==0){
-                x=x*x;
-                nn=nn/2;
-            }
-            else{
-                ans=ans*x;
-                nn=nn-1;
-            }
-        }
+        long long int nn=n;
         if(n<0){
-            return (double)(1.0)/(double)(ans);
+            
+            nn=-1*nn;
+            x=1/x;
         }
-        return ans;
+        double ans=myPow(x,nn/2);
+        if(nn%2==0){
+            return ans*ans;
+        }
+        else{
+            return x*ans*ans;
+        }
     }
 };
