860182e1039a06833816897e8650b893d4666780
Shrishti Mishra <shubhiarnm@gmail.com>
2025-08-08 17:25:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.31%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,35 +1,12 @@
 class Solution {
 public:
-    string convert2bin(int n){
-        string ans="";
-        while(n){
-            if(n%2==0){
-                ans+="0";
-            }
-            else{
-                ans+="1";
-            }
-            n=n/2;
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
-
     bool isPowerOfTwo(int n) {
-        string s=convert2bin(n);
-        int c=0;
-        if(n<0){
+        if(n<=0){
             return false;
         }
-        for (int i=0;i<s.size();i++){
-            if(s[i]=='1'){
-                c++;
-            }
-
+        if(n&(n-1)){
+            return false;
         }
-        if(c==1){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
