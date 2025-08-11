f861687dde335366ffc2f14f1d726232cd7c3be8
Shrishti Mishra <shubhiarnm@gmail.com>
2025-08-11 16:34:58 +0530

    Time: 0 ms (100%), Space: 12 MB (14.23%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -3,31 +3,30 @@ public:
     int countStudents(vector<int>& st, vector<int>& san) {
         int n=st.size();
         queue<int> stu;
-        stack<int> snd;
+        
         for (int i=0;i<n;i++){
             stu.push(st[i]);
         }
-        for (int i=n-1;i>=0;i--){
-            snd.push(san[i]);
-        }
+        int j=0;
         int c=0;
         int i=0;
 
-        while(!stu.empty()){
-            if(i>n*n){
-                break;
-            }
-            if(snd.top()==stu.front()){
-                snd.pop();
+        while(!stu.empty() && i<stu.size()){
+            
+            if(san[j]==stu.front()){
+                
                 stu.pop();
+                j++;
                 c++;
+                i=0;
             }
             else{
                 int rem=stu.front();
                 stu.pop();
                 stu.push(rem);
+                i++;
             }
-            i++;
+            
             
         }
         return n-c;
