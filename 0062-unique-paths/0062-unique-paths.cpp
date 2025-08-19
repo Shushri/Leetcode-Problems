ba7c51922b479ef47b2c5aa99d4c044ad4a87f28
Shrishti Mishra <shubhiarnm@gmail.com>
2025-08-19 20:25:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    int uniquePaths(int m, int n) {
+          int r=min(m-1,n-1);
+          int q=(m+n-2);
+          long long ans=1;
+          for(int i=1;i<=r;i++){
+            ans*=(q-r+i);
+          }
+          for(int i=1;i<=r;i++){
+            ans/=i;
+          }
+          return (int)ans;
+    }
+};
