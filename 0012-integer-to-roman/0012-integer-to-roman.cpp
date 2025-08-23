7700296b985b06d6290af972f29710a1d8e0234d
Shrishti Mishra <shubhiarnm@gmail.com>
2025-08-23 11:21:53 +0530

    Time: 0 ms (100%), Space: 8.2 MB (94.6%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,47 +1,60 @@
 class Solution {
 public:
     string intToRoman(int n) {
-        unordered_map <int,string> mpp; 
-        mpp[1]="I";
-        mpp[2]="II";
-        mpp[3]="III";
-        mpp[4]="IV";
-        mpp[5]="V";
-        mpp[6]="VI";
-        mpp[7]="VII";
-        mpp[8]="VIII";
-        mpp[9]="IX";
-        mpp[10]="X";
-        mpp[20]="XX";
-        mpp[30]="XXX";
-        mpp[40]="XL";
-        mpp[50]="L";
-        mpp[60]="LX";
-        mpp[70]="LXX";
-        mpp[80]="LXXX";
-        mpp[90]="XC";
-        mpp[100]="C";
-        mpp[200]="CC";
-        mpp[300]="CCC";
-        mpp[400]="CD";
-        mpp[500]="D";
-        mpp[600]="DC";
-        mpp[700]="DCC";
-        mpp[800]="DCCC";
-        mpp[900]="CM";
-        mpp[1000]="M";
-        mpp[2000]="MM";
-        mpp[3000]="MMM";
-
-        string ans="";
-        int i=1;
-        while(n){
-            int a=n%10;
-            int b=a*i;
-            ans=mpp[b]+ans;
-            n=n/10;
-            i*=10;
+        string s="";        
+        while(n>=1000){
+            n=n-1000;
+            s+="M";
         }
-        return ans;
+        while(n>=900){
+            n=n-900;
+            s+="CM";
+        }
+        while(n>=500){
+            n=n-500;
+            s+="D";
+        }
+        while(n>=400){
+            n=n-400;
+            s+="CD";
+        }
+        while(n>=100){
+            n=n-100;
+            s+="C";
+        }
+        while(n>=90){
+            n=n-90;
+            s+="XC";
+        }
+        while(n>=50){
+            n=n-50;
+            s+="L";
+        }
+        while(n>=40){
+            n=n-40;
+            s+="XL";
+        }
+        while(n>=10){
+            n=n-10;
+            s+="X";
+        }
+        while(n>=9){
+            n=n-9;
+            s+="IX";
+        }
+        while(n>=5){
+            n=n-5;
+            s+="V";
+        }
+        while(n>=4){
+            n=n-4;
+            s+="IV";
+        }
+        while(n>=1){
+            n=n-1;
+            s+="I";
+        }
+        return s;
+
     }
 };
