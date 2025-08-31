50dd96a41a8f0326fa506cadf0eca77e462c954d
Shrishti Mishra <shubhiarnm@gmail.com>
2025-08-31 15:06:41 +0530

    Time: 0 ms (100%), Space: 22.6 MB (64.22%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -3,82 +3,29 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int r=mat.size();
         int c=mat[0].size();
-        vector<vector<int>> red;
-        vector<vector<int>> yel;
-        for(int i=0;i<r;i+=2){
-            int r1=i;
-            int c1=0;
-            vector<int> d;
-            while(r1>=0 && c1<c){
-                d.push_back(mat[r1--][c1++]);
-
+        vector<int> ans;
+        for(int i=0;i<(r+c-1);i++){
+            
+            if(i%2==0){
+                int r1=i<r?i:(r-1);
+                int c1=i-r1;
+                while(r1>=0 && c1<c){
+                    ans.push_back(mat[r1--][c1++]);
+                }
             }
-            red.push_back(d);
+            else{
+                int c1=i<c?i:c-1;
+                int r1=i-c1;
+                while(r1<r && c1>=0){
+                    ans.push_back(mat[r1++][c1--]);
+                }
+            }
+            
+            
 
         }
         
-        for(int i=r&1?2:1;i<c;i+=2){
-            int r1=r-1;
-            int c1=i;
-            vector<int> d;
-            while(c1<c && r1>=0){
-                d.push_back(mat[r1--][c1++]);
-
-            }
-            red.push_back(d);
-            
-        }
-        // for(auto ele:red){
-        //     for(auto a:ele){
-        //         cout<<a<<" ";
-        //     }
-        // }
-        // cout<<endl;
-
-        for(int i=1;i<c;i+=2){
-            int r1=0;
-            int c1=i;
-            vector<int> d;
-            while(c1>=0 && r1<r){
-                d.push_back(mat[r1++][c1--]);
-
-            }
-            yel.push_back(d);
-
-        }
-        for(int i=c&1?1:2;i<r;i+=2){
-            int r1=i;
-            int c1=c-1;
-            vector<int> d;
-            while(r1<r && c1>=0){
-                d.push_back(mat[r1++][c1--]);
-
-            }
-            yel.push_back(d);
-            
-        }
-        // for(auto ele:yel){
-        //     for(auto a:ele){
-        //         cout<<a<<" ";
-        //     }
-        // }
-        // cout<<endl;
-        vector<int> ans;
-        for(int i=0;i<red.size();i++){
-            if(i<red.size()){
-                    for(int j=0;j<red[i].size();j++){
-                           ans.push_back(red[i][j]);
-                    }
-            }
-            if(i<yel.size()){
-                    for(int j=0;j<yel[i].size();j++){
-                            ans.push_back(yel[i][j]);
-                    }
-            }
-            
-            
-            
-        }
+        
         return ans;
 
     }
