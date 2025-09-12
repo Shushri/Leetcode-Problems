fb29a14238b4455dd249bb4b41b096279bddf7e0
Shrishti Mishra <shubhiarnm@gmail.com>
2025-09-12 13:31:56 +0530

    Time: 15 ms (18.28%), Space: 19.1 MB (53.73%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int c=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                c++;
+            }
+        }
+        if(c==0){
+            return false;
+        }
+        return true;
+    }
+};
