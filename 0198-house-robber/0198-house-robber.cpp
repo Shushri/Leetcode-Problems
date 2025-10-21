b7553159ca00cb18649e652f9610563a00818c9b
Shrishti Mishra <shubhiarnm@gmail.com>
2025-10-21 20:57:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int f(int ind,vector<int>& nums,int n,vector<int>&dp){
-        if(ind==0){
-            return nums[ind];
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
 
-        int pick=nums[ind]+f(ind-2,nums,n,dp);
-        int npick=0+f(ind-1,nums,n,dp);
 
-        return dp[ind]= max(pick,npick);
-    }
     int rob(vector<int>& nums) {
-        //this is a recurrence subsesquence problem with one extra constraint that the elements cant be take adjacent
-        vector<int> dp(nums.size(),-1);
-        return f(nums.size()-1,nums,nums.size(),dp);
+        //converting to tabulation form
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            } 
+            int npick=0+dp[i-1];
+            dp[i]=max(pick,npick);
+        }
+        return dp[n-1];
         
     }
 };
