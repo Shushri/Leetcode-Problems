b667d73c2113a6e4da61c21dfd9c349638087955
Shrishti Mishra <shubhiarnm@gmail.com>
2025-10-21 21:02:43 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,23 +4,19 @@ public:
 
     int rob(vector<int>& nums) {
         //converting to tabulation form
-        //space optimization
         int n=nums.size();
-        
-        int prev2=nums[0];
-        int prev1=nums[0];
-        int curr=nums[0];
+        vector<int> dp(n,-1);
+        dp[0]=nums[0];
+
         for(int i=1;i<n;i++){
             int pick=nums[i];
             if(i>1){
-                pick+=prev2;
+                pick+=dp[i-2];
             } 
-            int npick=0+prev1;
-            curr=max(pick,npick);
-            prev2=prev1;
-            prev1=curr;
+            int npick=0+dp[i-1];
+            dp[i]=max(pick,npick);
         }
-        return prev1;
+        return dp[n-1];
         
     }
 };
