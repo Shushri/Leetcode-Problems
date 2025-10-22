02fb668da188a0d34c6fca2011655e264914ee18
Shrishti Mishra <shubhiarnm@gmail.com>
2025-10-22 23:35:48 +0530

    Time: 0 ms (100%), Space: 10.8 MB (56.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    int f1(int ind,vector<int>& nums,int n,vector<int>&dp1){
-        if(ind==0){
-            return nums[ind];
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp1[ind]!=-1) return dp1[ind];
-
-        int pick=nums[ind]+f1(ind-2,nums,n,dp1);
-        int npick=0+f1(ind-1,nums,n,dp1);
-
-        return dp1[ind]= max(pick,npick);
+    int f(int ind, vector<int>& nums, vector<int>& dp){
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums[ind] + f(ind-2, nums, dp);
+        int npick = f(ind-1, nums, dp);
+        return dp[ind] = max(pick, npick);
     }
-    int f2(int ind,vector<int>& nums,int n,vector<int>&dp2){
-        if(ind==1){
-            return nums[ind];
-        }
-        if(ind<1){
-            return 0;
-        }
-        if(dp2[ind]!=-1) return dp2[ind];
 
-        int pick=nums[ind]+f2(ind-2,nums,n,dp2);
-        int npick=0+f2(ind-1,nums,n,dp2);
-
-        return dp2[ind]= max(pick,npick);
-    }
     int rob(vector<int>& nums) {
-        //this is a recurrence subsesquence problem with one extra constraint that the elements cant be take adjacent
-        if(nums.size()==1){
-            return nums[0];
-        }
-        vector<int> dp1(nums.size(),-1);
-        int l= f1(nums.size()-2,nums,nums.size(),dp1);
-        vector<int> dp2(nums.size(),-1);
-        int r= f2(nums.size()-1,nums,nums.size(),dp2);
-        return max(l,r);
+        int n = nums.size();
+        if(n == 1) return nums[0]; // edge case: only one house
+        // Case 1: exclude last house
+        vector<int> dp1(n, -1);
+        int case1 = f(n-2, nums, dp1);
+        // Case 2: exclude first house
+        vector<int> dp2(n, -1);
+        vector<int> nums2(nums.begin()+1, nums.end()); // new vector for 1..n-1
+        int case2 = f(nums2.size()-1, nums2, dp2);
+
+        return max(case1, case2);
     }
 };
