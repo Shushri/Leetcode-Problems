719392c31e1a9b66947b37b48daa029cfa601dd2
Shrishti Mishra <shubhiarnm@gmail.com>
2025-12-18 11:25:38 +0530

    Time: 21 ms (13.1%), Space: 58.6 MB (11.02%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -6,32 +6,26 @@ public:
         int i=n1-1;
         int j=n2-1;
         int c=0;
-        vector<int> ans(max(n1,n2)+1);
-        int k=ans.size()-1;
+        string ans="";
         while(i>=0 && j>=0){
             int sm=(num1[i--]-'0')+(num2[j--]-'0')+c;
-            ans[k--]=sm%10;
+            ans=(char)((sm%10)+'0')+ans;
             c=sm/10;
         }
         while(i>=0 ){
             int sm=(num1[i--]-'0')+c;
-            ans[k--]=sm%10;
+            ans=(char)((sm%10)+'0')+ans;
             c=sm/10;
         }
         while( j>=0){
             int sm=(num2[j--]-'0')+c;
-            ans[k--]=sm%10;
+            ans=(char)((sm%10)+'0')+ans;
             c=sm/10;
         }
-        ans[k]=c;
-        string anss="";
-        for(int i=ans.size()-1;i>=0;i--){
-            if(i==0 && ans[i]==0){
-                continue;
-            }
-            anss=(char)(ans[i]+'0')+anss;
+        if(c!=0){
+            ans=(char)(c+'0')+ans;
         }
-        return anss;
+        return ans;
 
 
     }
