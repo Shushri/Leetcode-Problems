756ab268edbe14ebc115c2ec055c70b34417903e
Shrishti Mishra <shubhiarnm@gmail.com>
2025-12-20 17:40:46 +0530

    Time: 0 ms (100%), Space: 10.6 MB (55.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,14 +4,12 @@ public:
         if(n==0){
             return {0};
         }
-        if(n==1){
-            return {0,1};
-        }
+        
         vector<int> ans(n+1);
         ans[0]=0;
-        ans[1]=1;
+        
         int c=0;
-        for(int i=2;i<=n;i++){
+        for(int i=1;i<=n;i++){
             if((i & (i-1))==0){
                 c=i;
                 
