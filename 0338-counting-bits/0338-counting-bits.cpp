9533b47c2d50e33f542617229256b11e44035406
Shrishti Mishra <shubhiarnm@gmail.com>
2025-12-20 17:39:23 +0530

    Time: 2 ms (33.45%), Space: 10.7 MB (35.59%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        if(n==0){
+            return {0};
+        }
+        if(n==1){
+            return {0,1};
+        }
         vector<int> ans(n+1);
-        for(int i=0;i<=n;i++){
-            ans[i] = __builtin_popcount(i);
+        ans[0]=0;
+        ans[1]=1;
+        int c=0;
+        for(int i=2;i<=n;i++){
+            if((i & (i-1))==0){
+                c=i;
+                
+            }
+            
+            ans[i]=ans[i-c]+1;
         }
         return ans;
     }
 };
+//0
+//0 1
+//0 1 1
+//0 1 1 2
+//0 1 1 2 1
+//0 1 1 2 1 2
