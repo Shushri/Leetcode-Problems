24d5a551eb770778d5ba187943b316363242af31
Shrishti Mishra <shubhiarnm@gmail.com>
2025-12-31 17:24:24 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.08%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long long s=floor(sqrt(1LL*2*n));
-        long long  p=s*(s+1)/2;
-        long long q=(s-1)*(s)/2;
-        if(p<=(long long)n){
-            return (int)s; 
-        }
-        else{
-            return (int)s-1;
-        }
-
+        double ans=-0.5 + 0.5 * sqrt(1+8*(double)n);
+        return ans;
         
     }
 };
+
+//
