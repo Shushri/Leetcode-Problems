83c5b096c5fac5dd106796287fbbfa9ebe6451ec
Shrishti Mishra <shubhiarnm@gmail.com>
2026-01-19 13:46:32 +0530

    Time: 30 ms (26.93%), Space: 107.3 MB (40.07%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
+        vector<int> ans(n);
+        stack<int> st;
+        
+        for(int i=0;i<n;i++){
+            
+                while(!st.empty() && t[i]>t[st.top()]){
+                    ans[st.top()]=i-st.top();
+                    st.pop();
+                }
+                st.push(i);
+            
+        }
+        while(!st.empty()){
+            ans[st.top()]=0;
+            st.pop();
+        }
+        return ans;
+    }
+};
