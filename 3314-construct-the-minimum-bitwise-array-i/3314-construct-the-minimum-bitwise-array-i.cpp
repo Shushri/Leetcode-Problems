14a69a2b546aeb21d00ca0cba0a28c5b5eba9fb2
Shrishti Mishra <shubhiarnm@gmail.com>
2026-01-20 11:48:20 +0530

    Time: 19 ms (10.06%), Space: 25 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            int b=0;
-            for(int j=0;j<nums[i];j++){
-                if((j | j+1)==nums[i]){
-                    nums[i]=j;
-                    b=1;
-                    break;
-                }
-            }
-            if(b==0){
+            if(nums[i]%2==0){
                 nums[i]=-1;
+            }
+            else{
+                //we look for trailing 1s and make the first 1 to 0 and keep other 1s as it is... 
+                //example... 100110111 ... 
+                //so the ans is 100110011
+                int temp=nums[i];
+                int c=0;
+                while((temp&1)!=0){
+                    temp=temp>>1;
+                    c++;
+                }
+                cout<<c<<endl;
+                int pp=1<<(c-1);
+                nums[i]=nums[i]&(~pp);
+
             }
         }
         return nums;
