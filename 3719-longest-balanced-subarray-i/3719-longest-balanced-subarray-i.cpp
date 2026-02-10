92814168a167e9d174a4ee567325cb33c9a813cc
Shrishti Mishra <shubhiarnm@gmail.com>
2026-02-10 19:09:18 +0530

    Time: 2008 ms (35.85%), Space: 522.2 MB (50.32%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            int e=0;
+            int o=0;
+            unordered_map<int,int> mpp;
+            for(int j=i;j<n;j++){
+                if(mpp[nums[j]]==0){
+                    if(nums[j]&1 ){
+                        o++;
+                    }
+                    else{
+                        e++;
+                    }
+                    mpp[nums[j]]++;
+                }
+                    
+                if(o==e){
+                    ans=max(ans,j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
