05aef7d1895372a808a141e0324cecd3bb25dc9d
Shrishti Mishra <shubhiarnm@gmail.com>
2026-02-16 17:15:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,18 +3,9 @@ public:
     int reverseBits(int n) {
         int p=0;
         for(int i=0;i<32;i++){
-            if((n&1)==1){
-                p=p<<1;
-                p=p|1;
-                
-                n=n>>1;
-            }
-            else{
-                p=p<<1;
-                p=p|0;
-                
-                n=n>>1;
-            }
+           p=p<<1;
+           p=(1&n)|p;
+           n=n>>1;
         }
         return p;
     }
