e198c76c88157035952845ff4507f1653bb04cb9
Shrishti Mishra <shubhiarnm@gmail.com>
2026-04-07 22:08:33 +0530

    Time: 18 ms (90.26%), Space: 23.4 MB (64.61%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,32 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root){
-            return NULL;
+        while (root) {
+            if (p->val < root->val && q->val < root->val) {
+                root = root->left;
+            } 
+            else if (p->val > root->val && q->val > root->val) {
+                root = root->right;
+            } 
+            else {
+                return root;
+            }
         }
-        else if(min(p->val,q->val)<root->val && max(p->val,q->val)>root->val){
-            return root;
-        }
-        else if(p->val<root->val && q->val<root->val){
-            return lowestCommonAncestor( root->left,  p,  q);
-        }
-        else if(p->val>root->val && q->val>root->val){
-            return lowestCommonAncestor( root->right,  p,  q);
-        }
-        else if(q->val==root->val || p->val==root->val){
-            return root;
-        }
-        return root;
-
+        return NULL;
     }
 };
