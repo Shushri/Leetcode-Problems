55f54ada9e13ffa119a36a448ed7058b494b1677
Shrishti Mishra <shubhiarnm@gmail.com>
2026-04-20 16:52:36 +0530

    Time: 3 ms (23.5%), Space: 14.6 MB (36.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<char>>& board,vector<vector<int>> &vis){
+    void dfs(int r,int c,vector<vector<char>>& board,vector<vector<int>> &vis,vector<int> &rr,vector<int> &cc){
         vis[r][c]=1;
         int n=board.size();
         int m=board[0].size();
-        if(r-1>=0 && board[r-1][c]=='O' && !vis[r-1][c]){
-            vis[r-1][c]=1;
-            dfs(r-1,c,board,vis);
-        }
-        if(c-1>=0 && board[r][c-1]=='O' && !vis[r][c-1]){
-            vis[r][c-1]=1;
-            dfs(r,c-1,board,vis);
-        }
-        if(r+1<n && board[r+1][c]=='O' && !vis[r+1][c]){
-            vis[r+1][c]=1;
-            dfs(r+1,c,board,vis);
-        }
-        if(c+1<m && board[r][c+1]=='O' && !vis[r][c+1]){
-            vis[r][c+1]=1;
-            dfs(r,c+1,board,vis);
-        }
 
+        for(int i=0;i<4;i++){
+            int r1=r+rr[i];
+            int c1=c+cc[i];
+            if(r1>=0 && r1<n && c1>=0 && c1<m && board[r1][c1]=='O' && !vis[r1][c1]){
+                vis[r1][c1]=1;
+                dfs(r1,c1,board,vis,rr,cc);
+            }
+        }
     }
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<int> rr={0,1,0,-1};
+        vector<int> cc={1,0,-1,0};
         for(int i=0;i<m;i++){
             if(board[0][i]=='O' && !vis[0][i]){
-                dfs(0,i,board,vis);
+                dfs(0,i,board,vis,rr,cc);
             }
 
             if(board[n-1][i]=='O' && !vis[n-1][i]){
-                dfs(n-1,i,board,vis);
+                dfs(n-1,i,board,vis,rr,cc);
             }
 
         }
         for(int i=0;i<n;i++){
             if(board[i][0]=='O' && !vis[i][0]){
-                dfs(i,0,board,vis);
+                dfs(i,0,board,vis,rr,cc);
             }
 
             if(board[i][m-1]=='O' && !vis[i][m-1]){
-                dfs(i,m-1,board,vis);
+                dfs(i,m-1,board,vis,rr,cc);
             }
 
         }
