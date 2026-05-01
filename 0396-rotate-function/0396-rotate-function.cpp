358ac4a3fd775643c8a66faa4b4be3c5f2ce9d24
Shrishti Mishra <shubhiarnm@gmail.com>
2026-05-01 09:48:02 +0530

    Time: 3 ms (53.85%), Space: 99.7 MB (51.69%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& A) {
+        int sum = 0, F = 0;
+        int n = A.size();
+
+        for (int i = 0; i < n; i++) {
+            sum += A[i];
+            F += i * A[i];
+        }
+
+        int maxi = F;
+
+        for (int i = 1; i < n; i++) {
+            F += sum - n * A[n - i];
+            maxi = max(maxi, F);
+        }
+
+        return maxi;
+    }
+};
+
+//sm=15
+//f=25
+//f1=25+15-4*6=16
+//f2=16+15-4*2=34-8=23
