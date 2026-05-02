349fb8db90dbf77461fd1d783e01f90dd4b56683
Shrishti Mishra <shubhiarnm@gmail.com>
2026-05-02 12:13:18 +0530

    Time: 14 ms (73.59%), Space: 39 MB (20.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,41 +3,44 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
+        vector<vector<int>> dis(n,vector<int>(m));
         vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,pair<int,int>>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
+                    dis[i][j]=0;
                 }
             }
         }
+        vector<int> dir={0,1,0,-1};
+        vector<int> dic={1,0,-1,0};
         while(!q.empty()){
-            pair<pair<int,int>,int> p=q.front();
-            q.pop();
-            int r=p.first.first;
-            int c=p.first.second;
-            int d=p.second;
-            
-            dis[r][c]=d;
-            if(r-1>=0 && mat[r-1][c]==1 && !vis[r-1][c]){
-                q.push({{r-1,c},d+1});
-                vis[r-1][c]=1;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                int d=q.front().first;
+                int r=q.front().second.first;
+                int c=q.front().second.second;
+                q.pop();
+
+                for(int j=0;j<4;j++){
+                    int rw=r+dir[j];
+                    int cl=c+dic[j];
+
+                    if(rw>=0 && rw<n && cl>=0 && cl<m && mat[rw][cl]==1 && !vis[rw][cl]){
+                        q.push({d+1,{rw,cl}});
+                        dis[rw][cl]=d+1;
+                        vis[rw][cl]=1;
+                    }
+                }
+
+
             }
-            if(c-1>=0 && mat[r][c-1]==1 && !vis[r][c-1]){
-                q.push({{r,c-1},d+1});
-                vis[r][c-1]=1;
-            }
-            if(r+1<n && mat[r+1][c]==1 && !vis[r+1][c]){
-                q.push({{r+1,c},d+1});
-                vis[r+1][c]=1;
-            }
-            if(c+1<m && mat[r][c+1]==1 && !vis[r][c+1]){
-                q.push({{r,c+1},d+1});
-                vis[r][c+1]=1;
-            }
+                
+
         }
         return dis;
+
     }
 };
