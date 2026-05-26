5adea83c1cd27f41cf2335145f4131d7c458a388
Shrishti Mishra <shubhiarnm@gmail.com>
2026-05-26 14:21:20 +0530

    Time: 5 ms (54.47%), Space: 19.2 MB (66.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    bool dfs(int nd,vector<vector<int>> &sub,vector<int> &vis,vector<int> &dfsvis){
-        vis[nd]=1;
-        dfsvis[nd]=1;
-        for(auto p:sub[nd]){
-            if(!vis[p]){
-                if(dfs(p,sub,vis,dfsvis)) return true;
-            }
-            else{
-                if(dfsvis[p]){
-                    return true;
-                }
-            }
-        }
-        dfsvis[nd]=0;
-        return false;
-    }
     bool canFinish(int n, vector<vector<int>>& pre) {
+        //kanh's algo (bfs on toopo sort)
+        vector<int> indeg(n);
         vector<vector<int>> sub(n);
         for(int i=0;i<pre.size();i++){
-            if(pre[i][1]==pre[i][0]){
+            if(pre[i][0]==pre[i][1]){
                 return false;
             }
             sub[pre[i][1]].push_back(pre[i][0]);
+            indeg[pre[i][0]]++;
         }
-        //using dfs
-        // for(auto ele : sub){
-        //     for(auto x:ele){
-        //         cout<<x<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        vector<int> vis(n,0);
-        vector<int> dfsvis(n,0);
-        bool ans=false;
+        queue<int> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,sub,vis,dfsvis)) return false;
+            if(indeg[i]==0){
+                q.push(i);
             }
         }
-        return true;
-
-
+        vector<int> ans;
         
+        while(!q.empty()){
+            int nd=q.front();
+            q.pop();
+            ans.push_back(nd);
+            for(auto p:sub[nd]){
+                indeg[p]--;
+                if(indeg[p]==0){
+                    q.push(p);
+                }
+            }
+        }
+        if(ans.size()<n){
+            return false;
+        }
+        return true;
     }
 };
