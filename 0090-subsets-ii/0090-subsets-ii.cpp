d76a6dae06ea5ad0f9e89c5dd13ebde38a2de6ee
Shrishti Mishra <shubhiarnm@gmail.com>
2026-06-22 16:54:14 +0530

    Time: 0 ms (100%), Space: 10.4 MB (84.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void fxn(int ind,vector<int> &temp,int n,vector<vector<int>> &ans,vector<int>& cand){
-            
-        ans.push_back(temp);
-            
-        for(int i=ind;i<n;i++){
-            
-            if(i>ind && cand[i]==cand[i-1]) continue;
-            temp.push_back(cand[i]);
-            fxn(i+1,temp,n,ans,cand);
-            temp.pop_back();
-        }   
+    void f(int ind,vector<int>& nums,vector<vector<int>> &ans,vector<int> &arr){
+            ans.push_back(arr);
+        
+        for(int i=ind;i<nums.size();i++){
+            if(i>ind && nums[i]==nums[i-1]){
+                continue;
+            }
+            arr.push_back(nums[i]);
+            f(i+1,nums,ans,arr);
+            arr.pop_back();
+        }
         
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& cand) {
-        sort(cand.begin(),cand.end());
-        int n=cand.size();
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        vector<int> temp;
-        
-        fxn(0,temp,n,ans,cand);
-        
-        return ans;   
+        vector<int> arr;
+        f(0,nums,ans,arr);
+        return ans;
     }
 };
