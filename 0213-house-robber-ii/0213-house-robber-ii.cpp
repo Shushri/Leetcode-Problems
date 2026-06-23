ce7e03574ae74cd2c15560dfdbfb7477387d693b
Shrishti Mishra <shubhiarnm@gmail.com>
2026-06-23 09:59:46 +0530

    Time: 1 ms (10.55%), Space: 10.9 MB (18.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind < 0) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int pick = nums[ind] + f(ind-2, nums, dp);
-        int npick = f(ind-1, nums, dp);
-        return dp[ind] = max(pick, npick);
+    int f(int ind,int mx,vector<int>& nums,vector<int>& dp){
+        if(ind>mx){
+            return 0;
+        }
+        if(ind==mx){
+            return nums[ind];
+        }
+        if(dp[ind]!=-1) return dp[ind];
+        int taken=nums[ind]+f(ind+2,mx,nums,dp);
+        int nttaken=0+f(ind+1,mx,nums,dp);
+
+        return dp[ind]=max(taken,nttaken);
     }
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0]; // edge case: only one house
-        // Case 1: exclude last house
-        vector<int> dp1(n, -1);
-        int case1 = f(n-2, nums, dp1);
-        // Case 2: exclude first house
-        vector<int> dp2(n, -1);
-        vector<int> nums2(nums.begin()+1, nums.end()); // new vector for 1..n-1
-        int case2 = f(nums2.size()-1, nums2, dp2);
-
-        return max(case1, case2);
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        vector<int> dp1(n+1,-1);
+        int case1=f(0,n-2,nums,dp1);
+        vector<int> dp2(n+1,-1);
+        int case2=f(1,n-1,nums,dp2);
+        return max(case1,case2);
     }
 };
