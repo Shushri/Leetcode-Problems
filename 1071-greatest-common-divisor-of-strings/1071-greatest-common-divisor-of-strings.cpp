40fc25bab7905d9a7e724afa07985b0403bee4c8
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-08 23:28:57 +0530

    Time: 0 ms (100%), Space: 9.7 MB (81.64%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,38 +1,10 @@
 class Solution {
 public:
-    bool check(string t,string s){
-        if(s.find(t)==string::npos){
-            return false;
-        }
-        if(s.size()%t.size()!=0){
-            return false;
-        }
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i%t.size()]){
-                return false;
-            }
-        }
-        return true;
-    }
     string gcdOfStrings(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        if(n<m){
-            swap(str1,str2);
-            swap(n,m);
+        if(str1+str2 != str2+str1){
+            return "";
         }
-        
-        string ans="";
-        string t="";
-        for(int i=0;i<m;i++){
-
-            t+=str2[i];
-            if(m%(i+1)==0){
-                if(check(t,str2) && check(t,str1)){
-                    ans=t;
-                }
-            }
-        }
-        return ans;
+        int gcdl = gcd(str1.size(),str2.size());
+        return str1.substr(0,gcdl);
     }
 };
