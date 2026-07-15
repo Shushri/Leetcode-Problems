155e3cdc35669379a8320842280f5401cebfff34
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-15 23:22:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.13%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -8,14 +8,16 @@ public:
         if(n<=2){
             return 1;
         }
-        vector<int> dp(n+1,-1);
         
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int a=0;
+        int b=1;
+        int c=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            int temp=c+b+a;
+            a=b;
+            b=c;
+            c=temp;
         }
-        return dp[n];
+        return c;
     }
 };
