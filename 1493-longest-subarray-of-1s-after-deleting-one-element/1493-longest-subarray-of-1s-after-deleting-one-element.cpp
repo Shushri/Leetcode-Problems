c95f4fef5bec8b531f23f141745b13cb6ad1d107
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-21 00:59:36 +0530

    Time: 0 ms (100%), Space: 60.1 MB (79.41%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<int> arr;
         int n=nums.size();
-        int i=0;
-        for(i;i<n;i++){
-            if(nums[i]==1){
-                break;
-            }
-        }
-        int oc=0;
-        int zc=0;
-        for(i;i<n;i++){
-            if(nums[i]==1){
-                if(zc>0) {arr.push_back(zc);zc=0;}
-                oc++;
-            }
-            else{
-                if(oc>0) {arr.push_back(oc);oc=0;}
-                zc++;
-            }
-        }
-        if(oc>0) {arr.push_back(oc);oc=0;}
-        if(zc>0) {arr.push_back(zc);zc=0;}
+        int left=0;
+        int z=0;
         int mx=0;
-        if(arr.empty()) return 0;
-        if(arr.size()==1 && arr[0]==n) return n-1;
-        for(int j=0;j<arr.size();j++){
-            if(j%2==1){
-                if(arr[j]==1 && j<(arr.size()-1)){
-                    mx=max(mx,arr[j+1]+arr[j-1]);
-                }
-
+        for(int right=0;right<n;right++){
+            if(nums[right]==0) z++;
+            while(z>1){
+                if(nums[left]==0) z--;
+                left++;
+                
             }
-            else{
-                mx=max(mx,arr[j]);
-            }
+            
+            mx=max(mx,right-left);
+            
+            
         }
-
         return mx;
-
-
     }
 };
