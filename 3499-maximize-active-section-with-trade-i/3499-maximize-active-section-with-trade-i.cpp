2c5924d7a02053a11d8274a3bc0864900f8a539f
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-21 12:05:57 +0530

    Time: 74 ms (50%), Space: 54.7 MB (97.3%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n=s.size();
-        vector<int> arr;
         int i;
         int ones=0;
         for(int p=0;p<n;p++){
@@ -11,39 +10,37 @@ public:
             }
         }
         if(ones==0 || ones==n) return ones;
-
+        int c10=0;
         for(i=0;i<n;i++){
             if(s[i]=='0'){
                 break;
             }
         }
-        int oc=0;
-        int zc=0;
-        for(i;i<n;i++){
-            if(s[i]=='1'){
-                oc++;
-                if(zc>0){arr.push_back(zc);
-                zc=0;}
-            }
-            else{
-                zc++;
-                if(oc>0){arr.push_back(oc);
-                oc=0;}
-            }
-        }
-
-        if(zc>0){arr.push_back(zc);
-            zc=0;
+        while(s[i]=='0'){
+            c10++;
+            i++;
         }
         int mx=ones;
-        for(int j=0;j<arr.size();j+=2){
-            if(j+1<arr.size()){
-                int cnt=ones-arr[j+1];//convert to 0
-                cnt+=arr[j]+arr[j+1]+arr[j+2];
-                mx=max(mx,cnt);
 
+        while(i<n){
+            int c11=0;
+            while(i<n && s[i]=='1'){
+                i++;
+                c11++;
             }
+            if(c11==0) break;
+            int c20=0;
+            while(i<n && s[i]=='0'){
+                i++;
+                c20++;
+            }
+            if(c20==0) break;
+            
+            mx=max(mx,ones+c20+c10);
+            c10=c20;
         }
+
         return mx;
+
     }
 };
