49399afa774d835ff721cf5508d2a1efdccc6756
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-26 15:55:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -1,17 +1,18 @@
 class RecentCounter {
 public:
-    
     queue<int> q;
+
     RecentCounter() {
         
     }
-    
+
     int ping(int t) {
         q.push(t);
-        
-        while(!q.empty() && t-q.front()>3000){
+
+        while (!q.empty() && q.front() < t - 3000) {
             q.pop();
         }
+
         return q.size();
     }
 };
