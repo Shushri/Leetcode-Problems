15ba57c7b776757cc5e6bbf2a7f8997a829f55fd
Shrishti Mishra <shubhiarnm@gmail.com>
2026-07-29 20:02:46 +0530

    Time: 0 ms (100%), Space: 120.5 MB (87.7%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,35 +10,33 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* h1) {
-        ListNode* h2=new ListNode(-1);
-        ListNode* temp1=h1;
-        ListNode* temp2=h2;
-        while(temp1){
-            int vl=temp1->val;
-            ListNode* nd=new ListNode(vl);
-            temp2->next=nd;
-            temp2=temp2->next;
-            temp1=temp1->next;
+    int pairSum(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* h1=NULL;
+        while(fast && fast->next){
+            h1=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        ListNode* prev=NULL;
-        temp2=h2->next;
-        while(temp2){
-            ListNode* nxt=temp2->next;
-            temp2->next=prev;
-            prev=temp2;
-            temp2=nxt;
+        ListNode* h2=slow;
+        h1->next=NULL;
+        ListNode* temp=head;
+        ListNode* prev2=NULL;
+        while(temp){
+            ListNode* nd=temp->next;
+            temp->next=prev2;
+            prev2=temp;
+            temp=nd;
         }
-        temp2=prev;
-        temp1=h1;
-        int mx=0;
-        while(temp1){
-            int vl1=temp1->val;
-            temp1=temp1->next;
-            int vl2=temp2->val;
-            temp2=temp2->next;
-            mx=max(mx,vl1+vl2);
+        int ans=0;
+        while(h1){
+            ans=max(ans,h1->val+h2->val);
+            h1=h1->next;
+            h2=h2->next;
         }
-        return mx;
+        return ans;
+
+
     }
 };
