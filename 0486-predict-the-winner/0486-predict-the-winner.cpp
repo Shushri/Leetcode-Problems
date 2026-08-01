f0cbe14a173f76b72d218a4c994f54246d53b8be
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-01 15:40:09 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& a, vector<vector<int>>& dp) {
-        if (dp[i][j] != -1) return dp[i][j];
-        if (i == j) return dp[i][j] = a[i];
+    bool predictTheWinner(vector<int>& A) {
+        int n = A.size();
+        if (~n & 1) return true;
 
-        return dp[i][j] = max(
-            a[i] - f(i + 1, j, a, dp),
-            a[j] - f(i, j - 1, a, dp)
-        );
-    }
+        vector<int> dp(n);
 
-    bool predictTheWinner(vector<int>& a) {
-        int n = a.size();
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = A[i];
+            for (int j = i + 1; j < n; j++)
+                dp[j] = max(A[i] - dp[j], A[j] - dp[j - 1]);
+        }
 
-        if (!(n & 1)) return true;
-
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        return f(0, n - 1, a, dp) >= 0;
+        return dp[n - 1] >= 0;
     }
 };
