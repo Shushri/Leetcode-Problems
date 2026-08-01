94a8e6a2fb0139898db0f1f5a6dc11ff149f50a4
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-01 16:30:27 +0530

    Time: 0 ms (100%), Space: 34.4 MB (16.5%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,31 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* findRight(TreeNode* root){
-        while(root->right)
-            root=root->right;
-        return root;
+        TreeNode* temp=root;
+        while(temp->right){
+            temp=temp->right;
+        }
+        return temp;
     }
-
     TreeNode* f1(TreeNode* root){
-        if(!root->left) return root->right;
-        if(!root->right) return root->left;
-
-        TreeNode* nd=findRight(root->left);
-        nd->right=root->right;
+        if(!root->left){
+            return root->right;
+        }
+        else if(!root->right){
+            return root->left;
+        }
+        else{
+            TreeNode* nd=findRight(root->left);
+            nd->right=root->right;
+        }
         return root->left;
     }
-
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL) return NULL;
-
-        if(root->val==key)
+        if(root==NULL) return root;
+        if(root->val==key){
             return f1(root);
-
-        if(key<root->val)
-            root->left=deleteNode(root->left,key);
-        else
-            root->right=deleteNode(root->right,key);
-
+        }
+       
+            if(root->val>key){
+                root->left= deleteNode(root->left,key);
+            }
+            else{
+                root->right= deleteNode(root->right,key);
+            }
+        
         return root;
     }
 };
