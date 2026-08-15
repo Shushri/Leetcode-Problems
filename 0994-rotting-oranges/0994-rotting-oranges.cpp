a810f0af1140427274dc61baec5e5f02880801a2
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-15 16:37:34 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,46 +4,46 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         queue<pair<int,int>> q;
+        vector<int> dr={0,1,0,-1};
+        vector<int> dc={1,0,-1,0};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                
             }
         }
-        vector<int> dir={0,1,0,-1};
-        vector<int> dic={1,0,-1,0};
         int ans=0;
         while(!q.empty()){
             int sz=q.size();
-            int f=0;
+            int fl=0;
             for(int i=0;i<sz;i++){
-                pair<int,int> tp=q.front();
+                int r=q.front().first;
+                int c=q.front().second;
                 q.pop();
-                int r=tp.first;
-                int c=tp.second;
+                
                 for(int j=0;j<4;j++){
-                    int rw=r+dir[j];
-                    int cl=c+dic[j];
-                    if(rw>=0 && rw<n && cl>=0 && cl<m &&  grid[rw][cl]==1){
+                    int rw=r+dr[j];
+                    int cl=c+dc[j];
+                    if(rw>=0 && rw<n && cl>=0 && cl<m && grid[rw][cl]==1){
                         grid[rw][cl]=2;
                         q.push({rw,cl});
-                        f=1;
+                        fl=1;
                     }
                 }
-                
             }
-            if(f==1) ans++;
+            if (fl==1) ans++;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     return -1;
                 }
-                
             }
         }
         return ans;
+
+
+
     }
 };
