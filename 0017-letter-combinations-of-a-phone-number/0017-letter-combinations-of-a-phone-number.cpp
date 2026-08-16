f35758e84bda0c505f8fdffcf86e31c8e2d26279
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-16 19:29:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    vector<string> ans;
-    
-    void f(int ind, string &digits, string curr, unordered_map<char, string> &mpp) {
-        if (ind == digits.size()) {
-            ans.push_back(curr);
-            return;
+    void f(int ind,string s,vector<string>&ans,string &digits,unordered_map<char,string> &mpp){
+        if(ind==digits.size()){
+            ans.push_back(s);
+            return ;
         }
-        
-        string letters = mpp[digits[ind]];
-        for (char c : letters) {
-            f(ind + 1, digits, curr + c, mpp);
+        for(auto ch:mpp[digits[ind]]){
+            f(ind+1,s+ch,ans,digits,mpp);
         }
     }
-    
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {}; // handle empty input
-        
-        unordered_map<char, string> mpp = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-        };
-        
-        f(0, digits, "", mpp);
+        unordered_map<char,string> mpp={{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
+        vector<string> ans;
+        f(0,"",ans,digits,mpp);
         return ans;
+        
     }
 };
