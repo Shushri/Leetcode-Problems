0e7da519773c4f4ed8bcc146809de0411e6d3d54
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-24 21:05:18 +0530

    Time: 0 ms (100%), Space: 10.5 MB (23.54%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,18 +1,71 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int n=a.size();
-        int m=b.size();
-        int p=1+m/n;
-        string s="";
-        for(int i=0;i<=p+1;i++){
-            if(s.find(b)!=string::npos){
-                return i;
+    vector<int> lps(string &p) {
+        int m = p.size();
+        vector<int> l(m);
+
+        int len = 0;
+        int i = 1;
+
+        while(i < m) {
+            if(p[i] == p[len]) {
+                l[i] = ++len;
+                i++;
             }
-            s+=a;
-
+            else if(len != 0) {
+                len = l[len - 1];
+            }
+            else {
+                i++;
+            }
         }
-        return -1;
 
+        return l;
+    }
+
+    bool kmp(string &s, string &p) {
+        int n = s.size();
+        int m = p.size();
+
+        vector<int> l = lps(p);
+
+        int i = 0, j = 0;
+
+        while(i < n) {
+            if(s[i] == p[j]) {
+                i++;
+                j++;
+
+                if(j == m)
+                    return true;
+            }
+            else if(j != 0) {
+                j = l[j - 1];
+            }
+            else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    int repeatedStringMatch(string a, string b) {
+        int k = (b.size() + a.size() - 1) / a.size();
+
+        string s;
+
+        for(int i = 0; i < k; i++)
+            s += a;
+
+        if(kmp(s, b))
+            return k;
+
+        s += a;
+
+        if(kmp(s, b))
+            return k + 1;
+
+        return -1;
     }
 };
