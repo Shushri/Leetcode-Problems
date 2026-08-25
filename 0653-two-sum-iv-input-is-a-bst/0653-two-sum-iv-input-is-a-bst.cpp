49b93eb4729c84410f86d74d546a1f3a1a558c39
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-25 11:03:07 +0530

    Time: 0 ms (100%), Space: 37.3 MB (45.69%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,35 +9,60 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTiterator{
+    private:
+        stack<TreeNode*> st;
+        bool reverse;
+    public:
+        BSTiterator(TreeNode* root,bool rev){
+            reverse=rev;
+            pushAll(root);
+            
+        }
+        bool hasNext(TreeNode* root){
+            return !st.empty();
+        }
+        int next(){
+            TreeNode* x=st.top();
+            st.pop();
+            if(reverse) pushAll(x->left);
+            else pushAll(x->right);
+            return x->val;
+        }
+    private:
+        void pushAll(TreeNode* root){
+            while(root!=NULL){
+                st.push(root);
+                if(reverse){
+                    
+                    root=root->right;
+                }
+                else{
+                    
+                    root=root->left;
+
+                }
+            }
+        }
+};
 class Solution {
 public:
-    vector<int> arr;
-    void inorder(TreeNode* root){
-        if(root==NULL){
-            return ;
-        }
-        inorder(root->left);
-        arr.push_back(root->val);
-        inorder(root->right);
-    }
-    bool findTarget(TreeNode* root, int k)
-         {
-            inorder(root);
-        int n=arr.size();
-        int l=0;int h=n-1;
-        
-        while(l<h){
-            int sm=arr[l]+arr[h];
-            if(sm==k){
-                return true;
-            }
-            else if(k<sm){
-                h--;
+    
+    bool findTarget(TreeNode* root, int k){
+          BSTiterator l(root,false);
+          BSTiterator r(root,true);
+            int i=l.next();
+            int j=r.next();
+          while(i<j){
+            int sm=i+j;
+            if(sm==k) return true;
+            else if(sm<k){
+                i=l.next();
             }
             else{
-                l++;
+                j=r.next();
             }
-        }
-        return false;
+          }
+          return false;
     }
 };
