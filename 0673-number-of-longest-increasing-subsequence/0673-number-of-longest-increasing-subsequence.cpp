6ae03fc98fbb34985acf1a2bf229d51065dddfc4
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-25 20:37:01 +0530

    Time: 25 ms (78.35%), Space: 17.4 MB (67.28%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-
-        int n = nums.size();
-
-        vector<int> dp(n, 1);
-        vector<int> size(n, 1);
-
-        int mx = 1;
-
-        for(int i = 0; i < n; i++) {
-
-            for(int j = 0; j < i; j++) {
-
-                if(nums[j] < nums[i]) {
-
-                    // Found a longer LIS ending at i
-                    if(dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                        size[i] = size[j];
+        int n=nums.size();
+        
+        vector<int> dp(n,1);
+        vector<int> size(n,1);
+        int mx=INT_MIN;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i] ){
+                    if(dp[j]+1>dp[i]){
+                        dp[i]=dp[j]+1;
+                        size[i]=size[j];
+                        
                     }
-
-                    // Found another LIS of same maximum length
-                    else if(dp[j] + 1 == dp[i]) {
-                        size[i] += size[j];
+                    else if(dp[j]+1==dp[i]){
+                        size[i]+=size[j];
                     }
                 }
             }
-
-            mx = max(mx, dp[i]);
-        }
-
-        int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(dp[i] == mx) {
-                ans += size[i];
+            if(dp[i]>mx){
+                mx=dp[i];
+                
             }
         }
-
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==mx){
+                ans+=size[i];
+            }
+        }
         return ans;
+
     }
 };
