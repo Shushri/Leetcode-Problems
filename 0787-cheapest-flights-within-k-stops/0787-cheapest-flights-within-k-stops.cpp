41a4d00cc7f8f35ccd835e2e279d7532b60b977c
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-26 23:27:24 +0530

    Time: 158 ms (5.01%), Space: 54.1 MB (5.02%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> arr(n);//{{{1,2},{3,4}},{}}
-        for(int i=0;i<flights.size();i++){
-            arr[flights[i][0]].push_back({flights[i][1],flights[i][2]});
-        }
-        vector<int> dis(n,INT_MAX);
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq; //{k,{node,price}}
-        pq.push({0,{src,0}});
-        dis[src]=0;
-        while(!pq.empty()){
-            int pr=pq.top().second.second;
-            int nd=pq.top().second.first;
-            int c=pq.top().first;
-            pq.pop();
-            if(c>k){
-                continue;
-            }
-            for(auto ele:arr[nd]){
-                int dest=ele.first;
-                int pp=ele.second;
-                if(dis[dest]>pp+pr){
-                    dis[dest]=pp+pr;
-                    pq.push({c+1,{dest,pp+pr}});
+    int findCheapestPrice(int n, vector<vector<int>>& flights,
+                          int src, int dst, int k) {
+
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        // At most k stops = at most k+1 flights
+        for(int i = 0; i <= k; i++) {
+
+            vector<int> temp = dist;
+
+            for(auto flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int price = flight[2];
+
+                if(dist[u] != INT_MAX) {
+                    temp[v] = min(temp[v], dist[u] + price);
                 }
             }
-        }
-        if(dis[dst]==INT_MAX){
-            return -1;
-        }
-        return dis[dst];
 
+            dist = temp;
+        }
+
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
