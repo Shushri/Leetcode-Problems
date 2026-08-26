0e7a68b03290309105f55dd92a2a13b700b22bfb
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-26 20:01:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.74%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int n=s.size();
-        s+='0';
-        int i=0;
-        int j=0;
-        int one=0;
-        int ans=INT_MAX;
-        int ind=-1;
-        vector<string> lex;
-        while(j<=n){            
-            if(one==k){
-                while(s[i]!='1'){
+        int n = s.size();
+        int i = 0;
+        int one = 0;
+        string ans = "";
+        for (int j = 0; j < n; j++) {
+            if (s[j] == '1')
+                one++;
+            while (one >= k) {
+                while (i <= j && s[i] == '0')
                     i++;
+                string curr = s.substr(i, j - i + 1);
+
+                if (ans == "" ||
+                    curr.size() < ans.size() ||
+                    (curr.size() == ans.size() && curr < ans)) {
+                    ans = curr;
                 }
-                if(j-i<ans){
-                    ans=j-i;
-                    ind=i;
-                }
-                else if(j-i==ans){
-                    if(s.substr(i,ans)<s.substr(ind,ans)){
-                        ind=i;
-                    }
-                }
+
                 one--;
                 i++;
-                
-                
             }
-            if(s[j]=='1'){
-                one++;
-                j++;
-            }
-            else{
-                j++;
-            }
-            
-        }      
-        string ss="";
-        if(ind==-1) return "";
-        for(int i=ind;i<ind+ans;i++){
-            ss+=s[i];
         }
-        return ss;
+
+        return ans;
     }
 };
-
-//100011001
-//i=0, j=0
-//one=1
-//i=0, j=5
-//one 3
-//i=1, j=5, one = 2
-//i=1,j=8, one 3
-//
