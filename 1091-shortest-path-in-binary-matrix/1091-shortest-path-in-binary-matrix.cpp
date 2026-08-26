12fcb8198f7b33e6d7806fb86a415d334877c21f
Shrishti Mishra <shubhiarnm@gmail.com>
2026-08-26 22:26:47 +0530

    Time: 5 ms (98.19%), Space: 23.5 MB (84.59%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,33 +2,38 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
-        queue<pair<int,pair<int,int>>> q;
-        if(grid[0][0]==1){
+        queue<pair<int,int>> q;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
         vector<int> dc={1,1,-1,-1,0,1,0,-1};
         vector<int> dr={-1,1,1,-1,1,0,-1,0};
-        dis[0][0]=1;
-        q.push({1,{0,0}});
+        int d=1;
+        q.push({0,0});
+        grid[0][0]=1;
         while(!q.empty()){
-            int d=q.front().first;
-            int i=q.front().second.first;
-            int j=q.front().second.second;
-            q.pop();
-            for(int p=0;p<8;p++){
-                int rw=i+dr[p];
-                int cl=j+dc[p];
-                if(rw<n && rw>=0 && cl<n && cl>=0 && grid[rw][cl]==0 && d+1<dis[rw][cl] ){
-                    q.push({d+1,{rw,cl}});
-                    dis[rw][cl]=d+1;
+            int sz=q.size();
+            while(sz--){
+                int i=q.front().first;
+                int j=q.front().second;
+                if(i==n-1 && j==n-1) return d;
+                q.pop();
+                for(int p=0;p<8;p++){
+                    int rw=i+dr[p];
+                    int cl=j+dc[p];
+                    if(rw<n && rw>=0 && cl<n && cl>=0 && grid[rw][cl]==0 ){
+                        q.push({rw,cl});
+                        grid[rw][cl]=1;
+                        
+                    }
                 }
             }
+            d++;
         }
-        if(dis[n-1][n-1]==INT_MAX){
+        
             return -1;
-        }
-        return dis[n-1][n-1];
+        
+        
         
     }
 };
