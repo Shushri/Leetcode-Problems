1bb42aaf134257dc2b69edcbb338b1ab5c084bc3
Shrishti Mishra <shubhiarnm@gmail.com>
2026-09-04 01:03:54 +0530

    Time: 578 ms (5.14%), Space: 226 MB (5.13%) - LeetHub

--- 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.cpp
+++ 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isPossible(int n, vector<vector<int>>& edges) {
         vector<int> deg(n+1,0);
-        map<vector<int>,int> mpp;
+        set<vector<int>> st;
 
         int m=edges.size();
         for(int i=0;i<m;i++){
             deg[edges[i][0]]++;
             deg[edges[i][1]]++;
-            mpp[edges[i]]++;
-            mpp[{edges[i][1],edges[i][0]}]++;
+            st.insert(edges[i]);
+            st.insert({edges[i][1],edges[i][0]});
         }
         vector<int> odd;
         for(int i=0;i<=n;i++){
@@ -26,12 +26,12 @@ public:
         if(odd.size()==2){
             int a=odd[0];
             int b=odd[1];
-            if(mpp[{a,b}]==0){
+            if(st.count({a,b})==0){
                 return true;
             }
             for(int i=1;i<=n;i++){
                 if(i!=a && i!=b){
-                    if(mpp[{a,i}]==0 && mpp[{b,i}]==0){
+                    if(st.count({a,i})==0 && st.count({b,i})==0){
                         return true;
                     }
                 }
@@ -45,13 +45,13 @@ public:
             int d=odd[3];
             
             
-            if(mpp[{a,b}]==0 && mpp[{c,d}]==0){
+            if(!st.count({a,b}) && !st.count({c,d})){
                 return true;
             }
-            if(mpp[{a,c}]==0 && mpp[{b,d}]==0){
+            if(!st.count({a,c}) && !st.count({d,b})){
                 return true;
             }
-            if(mpp[{a,d}]==0 && mpp[{c,b}]==0){
+            if(!st.count({a,d}) && !st.count({c,b})){
                 return true;
             }
             return false;
@@ -68,3 +68,4 @@ public:
 //then for these 2 and 4 nodes check all pairs if they dont already exist.
 //and 4 me bhi bas 1 ke liye saare check kro if its pair doesnt already exist... check for left 2...
 //if there are 2 and they are connected then we can have another node that is even and is not conn to a and b we can conn both and and b to c
+//4 ke case me aisa kr nhi skte... because 2 se zyada edges add nhi krni
