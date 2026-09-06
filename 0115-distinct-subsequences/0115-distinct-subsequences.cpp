12d625d00e252dc0ade188e21728ff2a9237b662
Shrishti Mishra <shubhiarnm@gmail.com>
2026-09-06 10:22:53 +0530

    Time: 35 ms (34.2%), Space: 26.9 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    
+    int f(int i,int j,string &s, string &t,vector<vector<int>> &dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]=f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
+        return dp[i][j]=f(i-1,j,s,t,dp); 
+    }
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(j>i) continue;
-                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][m];
+        int n1=s.size();
+        int n2=t.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2,-1));
+        return f(n1-1,n2-1,s,t,dp);
     }
 };
+//needs dp
+//need to see the indexes of both
+//if equal chars at indexes... we will move both pointers or we can move for s only
+//nt eql ... move only for s
+//if we reach the end of t then add 1
+//if we reach end of s then 0;
