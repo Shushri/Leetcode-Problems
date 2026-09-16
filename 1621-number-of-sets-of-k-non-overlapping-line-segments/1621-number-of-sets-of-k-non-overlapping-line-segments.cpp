ac09db3d0c9635b3570ad622f2513fd05f7e8418
Shrishti Mishra <shubhiarnm@gmail.com>
2026-09-16 10:10:59 +0530

    Time: 67 ms (48.6%), Space: 16.7 MB (57.48%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,65 +1,55 @@
-class Solution {
-public:
-    const int M = 1e9 + 7;
 
-    int dp[1002][1002];
-    int prefixDp[1002][1002];
-
-    int mod(int a) {
-        return (a % M + M) % M;
+class Solution { 
+public: 
+    const int M=1e9+7; 
+    
+    int modulo(int a){ 
+        return (a%M + M)%M; 
+    } 
+    
+    int addValues(int a, int b){ 
+        return modulo(modulo(a)+modulo(b)); 
+    } 
+    
+    int multiplyValues(int a, int b){ 
+        return modulo(modulo(a)*modulo(b)); 
     }
-
-    int add(int a, int b) {
-        return (1LL * a + b) % M;
-    }
-
-    int multiply(int a, int b) {
-        return 1LL * a * b % M;
-    }
-
-    // Returns sum of ways for 1...n points
-    int prefixWays(int n, int k) {
-        if (n <= 0)
-            return 0;
-
-        if (prefixDp[n][k] != -1)
-            return prefixDp[n][k];
-
-        return prefixDp[n][k] =
-            add(prefixWays(n - 1, k), countWays(n, k));
-    }
-
-    // Returns number of ways to make k segments using n points
-    int countWays(int n, int k) {
-
-        if (k == 0)
-            return 1;
-
-        if (n <= 1 || n - 1 < k)
-            return 0;
-
-        if (dp[n][k] != -1)
-            return dp[n][k];
-
-        // Only one segment
-        if (k == 1)
-            return dp[n][k] = 1LL * n * (n - 1) / 2 % M;
-
-        int total = 0;
-
-        // Don't use the last point
-        total = add(total, countWays(n - 1, k));
-
-        // Make a segment ending at the last point
-        total = add(total, prefixWays(n - 1, k - 1));
-
-        return dp[n][k] = total;
-    }
-
-    int numberOfSets(int n, int k) {
-        memset(dp, -1, sizeof(dp));
-        memset(prefixDp, -1, sizeof(prefixDp));
-
-        return countWays(n, k);
-    }
+    
+    int dp[1002][1002]; 
+    int prefDp[1002][1002]; 
+    
+    int prefixWays(int n, int k){ 
+        if(n<2) 
+            return 0; 
+        if(prefDp[n][k]!=-1) 
+            return prefDp[n][k]; 
+        return prefDp[n][k]=addValues(prefixWays(n-1,k),countWays(n,k)); 
+    } 
+    
+    int countWays(int n, int k){ 
+        if(k==1){ 
+            return dp[n][k]=(n*(n-1))/2; 
+        } 
+        if(n==1) 
+            return 0; 
+        if(n-1==k) 
+            return 1; 
+        if(n-1<k) 
+            return 0; 
+        if(dp[n][k]!=-1) 
+            return dp[n][k]; 
+        
+        int total=0; 
+        total=addValues(total,countWays(n-1,k)); 
+        total=addValues(total,prefixWays(n-1,k-1)); 
+        
+        return dp[n][k]=total; 
+    } 
+    
+    int numberOfSets(int n, int k){ 
+        memset(dp,-1,sizeof(dp)); 
+        memset(prefDp,-1,sizeof(prefDp)); 
+        return countWays(n,k); 
+    } 
 };
+
