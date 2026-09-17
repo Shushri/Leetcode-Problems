9cb219a8f32ae383a6ad20d3e6e6697d42b7649c
Shrishti Mishra <shubhiarnm@gmail.com>
2026-09-17 12:48:02 +0530

    Time: 3 ms (74.78%), Space: 13.6 MB (89.81%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
         int n=graph.size();
-        queue<vector<int>> q;
-        q.push({0});
-        while(!q.empty()){
-            int sz=q.size();
-            while(sz--){
-                vector<int> arr=q.front();
-                q.pop();
-                int x=arr.back();
-                int f=0;
-                for(auto ele:graph[x]){
-                    f=1;
-                    vector<int> temp=arr;
-                    arr.push_back(ele);
-                    if(ele==n-1){
-                        ans.push_back(arr);
-                    }
-                    else q.push(arr);
-                    arr=temp;
-                }
-                if(f==0 && arr.back()==n-1){
-                    ans.push_back(arr);
-                }
-
-            }
-        }
-        return ans;
+        vector<vector<int>>path;
+        vector<int>p;
+         dfs(path,p,graph,0,n);
+        
+        return path;
     }
+    void dfs(vector<vector<int>>&path,vector<int>&p,vector<vector<int>> &graph,int i,int n){
+      p.push_back(i);
+      if(i==n-1){
+        path.push_back(p);
+        p.pop_back();
+        return;
+      }
+     for(int j: graph[i]) dfs(path,p,graph,j,n);
+     p.pop_back();
+      }
 };
